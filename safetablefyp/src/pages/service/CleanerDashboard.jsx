5fdb83707115bbda183dd653eedc004c164ef6ef
Awais <awaisariff004@gmail.com>
2026-06-06 01:02:09 +0500

    Update CleanerDashboard.jsx

--- safetablefyp/src/pages/service/CleanerDashboard.jsx
+++ safetablefyp/src/pages/service/CleanerDashboard.jsx
@@ -186,7 +186,7 @@ const CleanerDashboard = () => {
 
                   return (
                     <motion.div
-                      key={req.id}
+                      key={item.id}
                       initial={{ opacity: 0, y: 20 }}
                       animate={{ opacity: 1, y: 0 }}
                       exit={{ opacity: 0, scale: 0.95 }}
