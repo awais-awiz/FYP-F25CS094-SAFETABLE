96e5e0e3bc9c506dc49366644356287b96830b48
Awais <awaisariff004@gmail.com>
2026-06-06 02:46:47 +0500

    Delete backend directory

--- backend/app/models/safepay_payment.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class SafepayPaymentCreate(BaseModel):
-    order_id: str
-    table_number: Optional[int] = None
-    description: Optional[str] = "S.A.F.E Table Payment"
