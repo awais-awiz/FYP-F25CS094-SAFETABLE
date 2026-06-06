b95d92eb3bdbe58e3f6e40b5066699c088870315
Awais <awaisariff004@gmail.com>
2026-06-06 08:54:58 +0500

    Update utils.js

--- safetablefyp/src/lib/utils.js
+++ safetablefyp/src/lib/utils.js
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+// Deterministic prep time based on order ID (15-45 minutes)
+export const getTargetPrepTime = (orderId) => {
+  if (!orderId) return 30;
+  let hash = 0;
+  for (let i = 0; i < orderId.length; i++) {
+    hash = orderId.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return 15 + (Math.abs(hash) % 31);
+};
